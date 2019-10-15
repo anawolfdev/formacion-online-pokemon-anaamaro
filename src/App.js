66418388b549db5fce6ec3d47a3e4693d0d90614
Ana Amaro <amarovazquezana@gmail.com>
2019-10-15 16:49:54 +0200

    first commit and prepare files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div className='app'>:D</div>;
+  }
+}
+
+export default App;

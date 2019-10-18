7b100cb05bc8ebcf94c08653ffa6d17fa5327f20
Ana Amaro <amarovazquezana@gmail.com>
2019-10-18 22:36:53 +0200

    fix docs

---
+++ docs/precache-manifest.f6fd5d10b9c0b0e581cf824f835755ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1491c82a28b2760a5e74c15e20a95dcc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d2b361d160e6de6fa4df",
+    "url": "./static/css/main.d1fcd6ee.chunk.css"
+  },
+  {
+    "revision": "9c5ed304ddfd767a8408",
+    "url": "./static/js/2.ec365c3d.chunk.js"
+  },
+  {
+    "revision": "d2b361d160e6de6fa4df",
+    "url": "./static/js/main.703821a7.chunk.js"
+  },
+  {
+    "revision": "8cc4bd03af553957594d",
+    "url": "./static/js/runtime-main.8a26ecb1.js"
+  }
+]);
